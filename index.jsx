585fdb5230b83afcc387a7d0308fad9fb2171bef
RJurasek <jurasek.rd@gmail.com>
2023-09-13 21:06:41 -0600

    add and style footer

--- index.jsx
+++ index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Navbar from './pages/Navbar'
+import Footer from './pages/Footer'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
@@ -17,6 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
