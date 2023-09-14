f68a372ef0995578b301bdee803bcc535144ee1a
RJurasek <jurasek.rd@gmail.com>
2023-09-14 16:50:04 -0600

    add layout component
    moved header + footer to component, added layout component and nested Routes

--- index.jsx
+++ index.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Navbar from './pages/Navbar'
-import Footer from './pages/Footer'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
